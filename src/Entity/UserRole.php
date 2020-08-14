266d96565723c15c79612a710e44c9fb9784c4aa
Kacper <kacperu770@gmail.com>
2020-08-14 21:34:52 +0200

    Add the first two entities User and UserRole

---
+++ src/Entity/UserRole.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRoleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRoleRepository::class)
+ */
+class UserRole
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $name = [];
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\User", mappedBy="userrole")
+     */
+    private $user;
+
+    public function __construct()
+    {
+        $this->user = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?array
+    {
+        return $this->name;
+    }
+
+    public function setName(array $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUser(): Collection
+    {
+        return $this->user;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->user->contains($user)) {
+            $this->user[] = $user;
+            $user->setUserrole($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->user->contains($user)) {
+            $this->user->removeElement($user);
+            // set the owning side to null (unless already changed)
+            if ($user->getUserrole() === $this) {
+                $user->setUserrole(null);
+            }
+        }
+
+        return $this;
+    }
+}
