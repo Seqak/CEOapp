65f5dbb2e19e4eb8efc44897832b71e4a818bb3c
Kacper <kacperu770@gmail.com>
2020-08-15 19:13:04 +0200

    Add login system to the project

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +10,8 @@ class DashboardController extends AbstractController
     /**
      * @Route("/", name="dashboard")
      */
-    public function index(UserRepository $userRepository)
+    public function index()
     {
-//        $user = $userRepository->findOneBy(array('email' => ))
-
         return $this->render('dashboard/index.html.twig', [
             'user'
         ]);
